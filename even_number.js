35849988e9d9ec323f68fa37504dc64cb3dfd69d
Bhanuprasad-2005p <2500040147@kluniversity.in>
2026-08-04 10:49:52 +0530

    Add files via upload

---
+++ even_number.js
@@ -0,0 +1,8 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 == 0) {
+    console.log(array[i]);
+  }
+}
+console.log("Even numbers in the array:", array.filter(num => num % 2 == 0));
